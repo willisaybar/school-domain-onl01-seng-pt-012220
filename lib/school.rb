08d3f3931eaf838ba3f636f2a9e7f9c06edd8a05
Willis Aybar <willisaybar@gmail.com>
2020-02-10 00:29:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,24 +1,30 @@
 class School
+  attr_accessor :name, :roster
 
-  # attr_accessor :school
-  # attr_reader :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
 
-  SCHOOLS = []
-  ROSTER = []
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end
 
-  def initialize(school)
-    @school = school
-    if !(SCHOOLS.include?(@school))
-      SCHOOLS << @school
+  def grade(level)
+    roster.detect do |x, y|
+      if x == level
+        return y
+      end
     end
-    end
-
-    def roster=(roster)
-      @roster = roster
-    end
-
-    def roster
-      @roster
-    end
+  end
+  
+def sort
+  nu_hash = {}
+  roster.each do |x, y|
+    nu_hash[x] = y.sort
+  end
+  nu_hash
+end
 
 end
