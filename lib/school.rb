c71ff28e05203d4e37eecfaab577823024a50754
Willis Aybar <willisaybar@gmail.com>
2020-02-10 00:19:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,20 @@
 class School
 
   attr_accessor :school
+  attr_reader :student
 
   def initialize(school)
     @school = school
+    end
+
+    def school
+      @school
+    end
+
+    ROSTER = []
+
+    def student=(student)
+      @student = student 
     end
 
 end
